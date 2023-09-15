d72fe59ba548e62126a50ff685647bf5ad80347c
cruscaya <ronaldo12roni@gmail.com>
2023-09-15 11:44:22 -0500

    carrusel de imagenes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:app_state/home_page.dart';
+import 'package:app_state/test1_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       // Configura la pantalla principal de la aplicación
-      home: HomePage(),
-     
+      //home: HomePage(),
+      home:Test1Page()
     ),
   );
 }
