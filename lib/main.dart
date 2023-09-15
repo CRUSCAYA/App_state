a7f33ca49bb95d0681367ed28ecc34377d0c7380
cruscaya <ronaldo12roni@gmail.com>
2023-09-15 11:30:23 -0500

    listado de nombres

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:app_state/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      // Configura la pantalla principal de la aplicación
+      home: HomePage(),
+     
+    ),
+  );
+}
